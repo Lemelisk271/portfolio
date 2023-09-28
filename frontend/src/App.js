b45307acc06ce6c58ded3f2963b914a1e8cea7e1
Zach Smith <zwsmith27@gmail.com>
2023-09-28 01:13:50 -0700

    added dark mode to session storage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux'
 import { Switch, Route } from 'react-router-dom'
 import { restoreUser } from './store/session'
 import { UltraDarkModeContext } from './context/UltraDarkContext'
+import { DarkModeContext } from './context/DarkModeContext'
 import LandingPage from './components/LandingPage'
 import Navigation from './components/Navigation'
 import Footer from './components/Footer'
@@ -12,10 +13,22 @@ function App() {
   const dispatch = useDispatch()
   const [isLoaded, setIsLoaded] = useState(false)
   const { ultraDarkMode } = useContext(UltraDarkModeContext)
+  const { darkMode, setDarkMode } = useContext(DarkModeContext)
 
   useEffect(() => {
     dispatch(restoreUser()).then(() => setIsLoaded(true))
   }, [dispatch])
+
+  useEffect(() => {
+    const darkSession = localStorage.getItem("darkMode")
+    if (darkSession === null) {
+      setDarkMode(false)
+    } else if (darkSession === 'true') {
+      setDarkMode(true)
+    } else {
+      setDarkMode(false)
+    }
+  }, [darkMode])
 
   return (
     <>
