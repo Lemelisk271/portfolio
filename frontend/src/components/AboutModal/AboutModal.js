edb81c6ebec8afb060adc69c72dba0d0c31b17e4
Zach Smith <zwsmith27@gmail.com>
2023-09-28 21:29:24 -0700

    added about modal

---
+++ frontend/src/components/AboutModal/AboutModal.js
@@ -0,0 +1,36 @@
+import { useContext, useEffect, useState } from 'react'
+import { DarkModeContext } from '../../context/DarkModeContext'
+
+const AboutModal = ({ user }) => {
+  const [about, setAbout] = useState([])
+  const [isLoaded, setIsLoaded] = useState(false)
+  const { darkMode } = useContext(DarkModeContext)
+
+  useEffect(() => {
+    const aboutArray = user.about.split("\n")
+    setAbout(aboutArray)
+    setIsLoaded(true)
+  }, [])
+
+  const aboutModalClass = "aboutModal" + (darkMode ? " aboutModal-dark" : " aboutModal-light")
+
+  return (
+    <div className={aboutModalClass}>
+      {isLoaded ? (
+        <>
+          <h1>About {user.firstName} {user.lastName}</h1>
+          {about.map((element, i) => (
+            <p key={i}>{element}</p>
+          ))}
+          <button>Edit About</button>
+        </>
+      ):(
+        <>
+          <h1>Loading...</h1>
+        </>
+      )}
+    </div>
+  )
+}
+
+export default AboutModal
