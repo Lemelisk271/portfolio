cb14f48d36d2d24c881bd521347ac2bbb5d85dca
Zach Smith <zwsmith27@gmail.com>
2023-10-02 15:40:30 -0700

    users can change the skill icon

--- backend/routes/api/skills.js
+++ backend/routes/api/skills.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const { requireAuth } = require('../../utils/auth.js')
 const { Skill } = require('../../db/models')
+const { singleMulterUpload, removeFileFromS3, singlePublicFileUpload } = require('../../awsS3.js')
 
 router.put('/:skillId', requireAuth, async (req, res, next) => {
   const skill = await Skill.findByPk(req.params.skillId)
@@ -16,6 +17,31 @@ router.put('/:skillId', requireAuth, async (req, res, next) => {
 
   skill.set(req.body)
 
+  await skill.save()
+
+  res.status(201).json(skill)
+})
+
+router.put("/:skillId/image", requireAuth, singleMulterUpload("image"), async (req, res, next) => {
+  const skill = await Skill.findByPk(req.params.skillId)
+
+  if (!skill) {
+    const err = new Error("Not Found")
+    err.status = 404
+    err.title = "Skill Not Found"
+    err.errors = {Message: "The requested skill couldn't be found."}
+    return next(err)
+  }
+
+  if (skill.icon.split(".")[0] === 'https://zwsmith-portfolio') {
+    removeFileFromS3(skill.icon)
+  }
+
+  const icon = await singlePublicFileUpload(req.file)
+
+  skill.set({
+    icon
+  })
   await skill.save()
 
   res.status(201).json(skill)
--- frontend/src/components/ChangeUserSkillIconModal/ChangeUserSkillIconModal.js
+++ frontend/src/components/ChangeUserSkillIconModal/ChangeUserSkillIconModal.js
@@ -1,7 +1,93 @@
-const ChangeUserSkillIconModal = () => {
+import { useContext, useState, useEffect } from 'react'
+import { DarkModeContext } from '../../context/DarkModeContext'
+import { csrfFetch } from '../../store/csrf'
+import { useModal } from '../../context/Modal'
+import { ResetContext } from '../../context/ResetContext'
+
+const ChangeUserSkillIconModal = ({ skill }) => {
+  const [image, setImage] = useState(null)
+  const [previewImage, setPreviewImage] = useState(null)
+  const [validationErrors, setValidationErrors] = useState({})
+  const [isSubmitted, setIsSubmitted] = useState(false)
+  const { darkMode } = useContext(DarkModeContext)
+  const { reset, setReset } = useContext(ResetContext)
+  const { closeModal } = useModal()
+
+  useEffect(() => {
+    const errors = {}
+
+    if (image === null) {
+      errors.image = "Please select an image"
+    }
+
+    setValidationErrors(errors)
+  }, [image])
+
+  useEffect(() => {
+    if (!image) {
+      setPreviewImage(null)
+      return
+    }
+
+    const objectUrl = URL.createObjectURL(image)
+    setPreviewImage(objectUrl)
+
+    return () => URL.revokeObjectURL(objectUrl)
+  }, [image])
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    setIsSubmitted(true)
+
+    if (Object.values(validationErrors).length > 0) return
+
+    const formData = new FormData()
+    formData.append("image", image)
+    const res = await csrfFetch(`/api/skills/${skill.id}/image`, {
+      method: 'PUT',
+      headers: {
+        "Content-Type": "multipart/form-data"
+      },
+      body: formData
+    })
+    if (res.ok) {
+      setReset(!reset)
+      closeModal()
+    } else {
+      const data = await res.json()
+      if (data && data.errors) {
+        setValidationErrors(data.errors)
+      }
+    }
+  }
+
+  const updateFile = (e) => {
+    const file = e.target.files[0]
+    if (file) setImage(file)
+  }
+
+  const skillIconClass = "changeUserSkillIcon" + (darkMode ? " changeUserSkillIcon-dark" : " changeUserSkillIcon-light")
+
   return (
-    <div>
-      <h1>Change User Skill Modal</h1>
+    <div className={skillIconClass}>
+      <h1>Change Icon For {skill.skill}</h1>
+      {image && <>
+        <p>Preview Image</p>
+        <img src={previewImage} alt={skill.skill}/>
+      </>}
+      {(isSubmitted && Object.values(validationErrors).length > 0) && <ul>
+          {Object.values(validationErrors).map((error, i) => (
+            <li key={i} className='error'>{error}</li>
+          ))}
+        </ul>}
+      <form onSubmit={handleSubmit}>
+        <input
+          type='file'
+          accept='image/*'
+          onChange={updateFile}
+        />
+        <button type='submit'>Save</button>
+      </form>
     </div>
   )
 }
