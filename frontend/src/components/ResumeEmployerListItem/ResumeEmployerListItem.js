b1bdc32d7375350e072b4ab7774630b34e8bccc1
Zach Smith <zwsmith27@gmail.com>
2023-10-04 15:07:58 -0700

    added the resume employer list item

--- frontend/src/components/ResumeEmployerListItem/ResumeEmployerListItem.js
+++ frontend/src/components/ResumeEmployerListItem/ResumeEmployerListItem.js
@@ -1,17 +1,52 @@
+import { useContext, useState, useEffect } from 'react'
+import { DarkModeContext } from '../../context/DarkModeContext'
+import { csrfFetch } from '../../store/csrf'
+
 const ResumeEmployerListItem = ({ employer }) => {
+  const [employerBullets, setEmployerBullets] = useState([])
+  const [isLoaded, setIsLoaded] = useState(false)
+  const { darkMode } = useContext(DarkModeContext)
+
   console.log(employer)
 
+  useEffect(() => {
+    const loadPage = async () => {
+      const res = await csrfFetch(`/api/resumes/employerbullet/${employer.id}`)
+      const bulletData = await res.json()
+      setEmployerBullets(bulletData)
+      setIsLoaded(true)
+    }
+    loadPage()
+  }, [])
+
+  const resumeEmployerClass = "resumeEmployerListItem" + (darkMode ? " resumeEmployerListItem-dark" : " resumeEmployerListItem-light")
+
   return (
-    <div className='resumeEmployerListItem'>
-      <div className="resumeEmployerListItem-header">
-        <div className="resumeEmployerListItem-headerInfo">
-          <h3>{employer.position}</h3>
-          <p>{employer.company}, {employer.location}</p>
-        </div>
-        <div className="resumeEmployerListItem-headerDates">
-          <p>{employer.startDate} - {employer.endDate}</p>
-        </div>
-      </div>
+    <div className={resumeEmployerClass}>
+      {isLoaded ? (
+        <>
+          <div className="resumeEmployerListItem-header">
+            <div className="resumeEmployerListItem-headerInfo">
+              <h3>{employer.position}</h3>
+              <p>{employer.company}, {employer.location}</p>
+            </div>
+            <div className="resumeEmployerListItem-headerDates">
+              <p>{employer.startDate} - {employer.endDate}</p>
+            </div>
+          </div>
+          <div className='resumeEmployerListItem'>
+            <ul>
+              {employerBullets.map((bullet, i) => (
+                <li key={i}>{bullet.bullet}</li>
+              ))}
+            </ul>
+          </div>
+        </>
+      ):(
+        <>
+          <h2>Loading...</h2>
+        </>
+      )}
     </div>
   )
 }
