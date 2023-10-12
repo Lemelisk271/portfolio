75f6815ccdf2f97654f287f88456e94f940846ce
Zach Smith <zwsmith27@gmail.com>
2023-10-12 16:09:53 -0700

    added edit resume title modal

---
+++ frontend/src/components/EditResumeTitleModal/EditResumeTitleModal.js
@@ -0,0 +1,9 @@
+const EditResumeTitleModal = () => {
+  return (
+    <div>
+      <h1>Edit Resume Title Modal</h1>
+    </div>
+  )
+}
+
+export default EditResumeTitleModal
--- frontend/src/components/ResumeProfilePage/ResumeProfilePage.js
+++ frontend/src/components/ResumeProfilePage/ResumeProfilePage.js
@@ -1,16 +1,48 @@
-import { useContext } from 'react'
+import { useContext, useState, useEffect } from 'react'
 import { useSelector } from 'react-redux'
+import { csrfFetch } from '../../store/csrf'
 import { DarkModeContext } from '../../context/DarkModeContext'
+import EditResumeTitleModal from '../EditResumeTitleModal'
+import OpenModalButton from '../OpenModalButton'
 
 const ResumeProfilePage = () => {
   const sessionUser = useSelector(state => state.session.user)
+  const [resume, setResume] = useState({})
+  const [isLoaded, setIsLoaded] = useState(false)
   const { darkMode } = useContext(DarkModeContext)
+
+  useEffect(() => {
+    const loadPage = async () => {
+      const res = await csrfFetch(`/api/resumes/${sessionUser.id}`)
+      const resumeData = await res.json()
+      setResume(resumeData)
+      console.log(resumeData)
+
+      setIsLoaded(true)
+    }
+    loadPage()
+  }, [])
 
   const resumeProfileClass = "resumeProfilePage" + (darkMode ? " resumeProfilePage-dark" : " resumeProfilePage-light")
 
   return (
     <div className={resumeProfileClass}>
-      <h1>{sessionUser.firstName} {sessionUser.lastName}'s Resume</h1>
+      {isLoaded ? (
+        <>
+          <h1>{sessionUser.firstName} {sessionUser.lastName}'s Resume</h1>
+          <div className='resumeProfilePage-title'>
+            <h2>Title: {resume.title}</h2>
+            <OpenModalButton
+              buttonText="Edit Title"
+              modalComponent={<EditResumeTitleModal />}
+            />
+          </div>
+        </>
+      ):(
+        <>
+          <h1>Loading...</h1>
+        </>
+      )}
     </div>
   )
 }
