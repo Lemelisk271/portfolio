d4022050366f12ebbc9fbf6cd6f1f5fe67af343a
Zach Smith <zwsmith27@gmail.com>
2023-10-25 16:06:33 -0700

    added resume employer list item

---
+++ frontend/src/components/ResumeProfileEmployerListItem/ResumeProfileEmployerListItem.js
@@ -0,0 +1,9 @@
+const ResumeProfileEmployerListItem = ({ employer }) => {
+  return (
+    <div className="resumeProfileEmployerListItem">
+      <p>{employer.company}</p>
+    </div>
+  )
+}
+
+export default ResumeProfileEmployerListItem
